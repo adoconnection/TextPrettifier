c330a247259601de0e829fb03be1c367cd5d37e6
Alexnader Selishchev <ADOConnection@gmail.com>
2020-01-14 17:05:26 +0300

    fixes

--- Pack.ps1
+++ Pack.ps1
@@ -1,4 +1,4 @@
 dotnet build -c Release
 dotnet test -c Release 
 dotnet pack -c Release -o artifacts TextPrettifier\TextPrettifier.csproj 
-dotnet nuget push artifacts\TextPrettifier.2020.01.nupkg --source https://www.nuget.org/api/v2/package
+dotnet nuget push artifacts\TextPrettifier.2020.1.0.nupkg --source https://www.nuget.org/api/v2/package
